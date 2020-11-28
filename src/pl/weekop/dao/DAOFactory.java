39e1f68d248317f82104ac0ba6048dd04509b76f
Arek <ark.bogda@gmail.com>
2020-11-28 18:47:29 +0100

    DAO structure

---
+++ src/pl/weekop/dao/DAOFactory.java
@@ -0,0 +1,11 @@
+package pl.weekop.dao;
+
+public abstract class DAOFactory {
+
+    public abstract DiscoveryDAO getDiscoveryDAO();
+
+    public abstract UserDAO getUserDAO();
+
+    public abstract VoteDAO getVoteDAO();
+
+}
---
+++ src/pl/weekop/dao/DiscoveryDAOImpl.java
@@ -0,0 +1,33 @@
+package pl.weekop.dao;
+
+import java.util.List;
+import pl.weekop.model.Discovery;
+
+public class DiscoveryDAOImpl implements DiscoveryDAO {
+
+    @Override
+    public Discovery create(Discovery newObject) {
+        return null;
+    }
+
+    @Override
+    public Discovery read(Long primaryKey) {
+        return null;
+    }
+
+    @Override
+    public boolean update(Discovery updateObject) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Long key) {
+        return false;
+    }
+
+    @Override
+    public List<Discovery> getAll() {
+        return null;
+    }
+
+}
