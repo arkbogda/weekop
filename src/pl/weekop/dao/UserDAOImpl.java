8b84519be9b8ec0b6c163b123bd4607597e1b86a
Arek <ark.bogda@gmail.com>
2020-12-09 17:40:45 +0100

    Login implementation

--- src/pl/weekop/dao/UserDAOImpl.java
+++ src/pl/weekop/dao/UserDAOImpl.java
@@ -1,6 +1,8 @@
 package pl.weekop.dao;
 
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -8,12 +10,18 @@ import org.springframework.jdbc.support.KeyHolder;
 import pl.weekop.model.User;
 import pl.weekop.util.ConnectionProvider;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDAOImpl implements UserDAO {
 
     private static final String CREATE_USER =
             "INSERT INTO user(username, email, password, is_active) VALUES(:username, :email, :password, :active);";
+    private static final String READ_USER =
+            "SELECT user_id, username, email, password, is_active FROM user WHERE user_id = :id";
+    private static final String READ_USER_BY_USERNAME =
+            "SELECT user_id, username, email, password, is_active FROM user WHERE username = :username";
 
     private NamedParameterJdbcTemplate template;
 
@@ -42,7 +50,10 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User read(Long primaryKey) {
-        return null;
+        User resultUser = null;
+        SqlParameterSource paramSource = new MapSqlParameterSource("id", primaryKey);
+        resultUser = template.queryForObject(READ_USER, paramSource, new UserRowMapper());
+        return resultUser;
     }
 
     @Override
@@ -62,6 +73,23 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public User getUserByUsername(String username) {
-        return null;
+        User resultUser = null;
+        SqlParameterSource paramSource = new MapSqlParameterSource("username", username);
+        resultUser = template.queryForObject(READ_USER_BY_USERNAME, paramSource, new UserRowMapper());
+        return resultUser;
+    }
+
+    private class UserRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(resultSet.getLong("user_id"));
+            user.setUsername(resultSet.getString("username"));
+            user.setEmail(resultSet.getString("email"));
+            user.setPassword(resultSet.getString("password"));
+            return user;
+        }
+
     }
 }
