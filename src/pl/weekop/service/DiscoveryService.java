042fbf0d7f0501e988ea2f66745dc749825a3834
Arek <ark.bogda@gmail.com>
2020-12-10 13:01:14 +0100

    Home page implementation

--- src/pl/weekop/service/DiscoveryService.java
+++ src/pl/weekop/service/DiscoveryService.java
@@ -6,7 +6,9 @@ import pl.weekop.model.Discovery;
 import pl.weekop.model.User;
 
 import java.sql.Timestamp;
+import java.util.Comparator;
 import java.util.Date;
+import java.util.List;
 
 public class DiscoveryService {
     public void addDiscovery(String name, String desc, String url, User user) {
@@ -25,5 +27,19 @@ public class DiscoveryService {
         discovery.setUser(userCopy);
         discovery.setTimestamp(new Timestamp(new Date().getTime()));
         return discovery;
+    }
+
+    public List<Discovery> getAllDiscoveries() {
+        return getAllDiscoveries(null);
+    }
+
+    public List<Discovery> getAllDiscoveries(Comparator<Discovery> comparator) {
+        DAOFactory factory = DAOFactory.getDAOFactory();
+        DiscoveryDAO discoveryDao = factory.getDiscoveryDAO();
+        List<Discovery> discoveries = discoveryDao.getAll();
+        if(comparator != null && discoveries != null) {
+            discoveries.sort(comparator);
+        }
+        return discoveries;
     }
 }
